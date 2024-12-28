0f463c5733b6194ba1e63f6e08d4f46cede17652
josip <josipkeresman@gmail.com>
2024-12-28 15:24:47 +0100

    Fix formatting in Subject.cs

--- Observer/Solution/Subject.cs
+++ Observer/Solution/Subject.cs
@@ -4,9 +4,7 @@ public abstract class Subject
 {
     private List<IObserver> _observers = new List<IObserver>();
     
-    
     public void AddObserver(IObserver observer) => _observers.Add(observer);
-    
     public void RemoveObserver(IObserver observer) => _observers.Remove(observer);
     public void Notify() => _observers.ForEach(observer => observer.Update());
 }
