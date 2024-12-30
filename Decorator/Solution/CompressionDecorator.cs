d2145b078a91773a996989d9e5694e82b2b80c3f
josip <josipkeresman@gmail.com>
2024-12-30 21:42:23 +0100

    Fix formatting in decorator design pattern

--- Decorator/Solution/CompressionDecorator.cs
+++ Decorator/Solution/CompressionDecorator.cs
@@ -5,11 +5,13 @@ public class CompressionDecorator : DataDecorator
     public CompressionDecorator(IData data) : base(data)
     {
     }
+    
    public override void Save(string data)
    {
       Console.WriteLine("Compressing data");
       var comppresed = Compress(data);
       _data.Save(comppresed);
    }
+   
    public string Compress(string data) => data.Substring(0, 9);
 }
--- Decorator/Solution/EncryptionDecorator.cs
+++ Decorator/Solution/EncryptionDecorator.cs
@@ -5,6 +5,7 @@ public class EncryptionDecorator : DataDecorator
     public EncryptionDecorator(IData data) : base(data)
     {
     }
+    
     public override void Save(string data)
     {
         var encrypted = Encrypt(data);
