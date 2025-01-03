606f78b6e63e7ff828e837e5f2c9e25a54143eaf
josip <josipkeresman@gmail.com>
2025-01-03 23:18:10 +0100

    Refactor command design pattern

--- Command/Solution/TurnOffCommand.cs
+++ Command/Solution/TurnOffCommand.cs
@@ -4,9 +4,7 @@ public class TurnOffCommand : ICommand
 {
     private readonly Light _light;
 
-    public TurnOffCommand(Light light)
-    {
-        _light = light;
-    }
+    public TurnOffCommand(Light light) => _light = light;
+    
     public void Execute() => _light.TurnOff();
 }
